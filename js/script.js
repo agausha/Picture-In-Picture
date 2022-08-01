a28800c8228162e94c2114e1d314c52c50419842
Isaac <uniqucode@gmail.com>
2022-08-01 10:39:41 +0100

    Added try & catch

--- js/script.js
+++ js/script.js
@@ -2,4 +2,6 @@ const videoElement = document.getElementById('video');
 const button = document.getElementById('button');
 
 // Prompt to select media stream, pass to video element, then play 
-async function selectMediaStream() {}
+async function selectMediaStream() {
+  try {} catch{}
+}
