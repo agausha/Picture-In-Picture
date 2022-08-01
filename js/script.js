edc66dd71ccf02734567354139503438ef27162f
Isaac <uniqucode@gmail.com>
2022-08-01 10:38:23 +0100

    Added a function

--- js/script.js
+++ js/script.js
@@ -1,2 +1,5 @@
 const videoElement = document.getElementById('video');
 const button = document.getElementById('button');
+
+// Prompt to select media stream, pass to video element, then play 
+async function selectMediaStream() {}
